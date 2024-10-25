8b71b506342ce265b1ee800255e4ce3ba57dd1ed
anastasia228-200 <anastasiya2006chernyavskaya@gmail.com>
2024-10-25 21:02:25 +0300

    практики а и в за урок 7 (не делала с)

---
+++ Course/2nd year/Lesson7/practice/practice_a_lesson7.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+class Program
+{
+    ////задача1
+    //static long Factorial(int n)
+    //{
+    //    if (n == 1)  // Базовый случай: факториал 1 равен 1
+    //    {
+    //        return 1;
+    //    }
+    //    return n * Factorial(n - 1);  // Рекурсивный вызов
+    //}
+
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine(Factorial(4));
+    //}
+
+    ////задача2
+    //static int Fibonacci(int n)
+    //{
+    //    if (n <= 1) return n;
+    //    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+    //}
+
+    //static void Main(string[] args)
+    //{
+    //    int n = 8;
+    //    Console.WriteLine(Fibonacci(n));
+    //}
+
+    ////задача3
+    //static void Main()
+    //{
+    //    string original = Console.ReadLine();
+    //    string reversed = ReverseString(original);
+    //    Console.WriteLine(reversed);
+    //}
+
+    //static string ReverseString(string str)
+    //{
+    //    if (str.Length == 0)
+    //        return str;
+
+    //    return str[str.Length - 1] + ReverseString(str.Substring(0, str.Length - 1));
+    //}
+
+
+    ////задача4
+    //static int SumArray(int[] arr, int length)
+    //{
+    //    if (length == 0)
+    //        return 0;
+
+    //    return arr[length - 1] + SumArray(arr, length - 1);
+    //}
+    //static void Main()
+    //{
+    //    int[] numbers = { 1, 2, 3, 4, 5 };
+    //    int sum = SumArray(numbers, numbers.Length);
+    //    Console.WriteLine($"Сумма элементов массива: {sum}");
+    //}
+
+    ////задача5
+    //static void Main()
+    //{
+    //    Console.Write("Введите первое число: ");
+    //    int a = int.Parse(Console.ReadLine());
+
+    //    Console.Write("Введите второе число: ");
+    //    int b = int.Parse(Console.ReadLine());
+
+    //    int gcd = GCD(a, b);
+    //    Console.WriteLine($"НОД чисел {a} и {b} равен: {gcd}");
+    //}
+
+    //static int GCD(int a, int b)
+    //{
+    //    if (b == 0)
+    //        return a;
+
+    //    return GCD(b, a % b);
+    //}
+
+    ////задача6
+    //static bool Palindrome(string str)
+    //{
+    //    if (str.Length <= 1)
+    //        return true;
+
+    //    if (str[0] != str[str.Length - 1])
+    //        return false;
+
+    //    return Palindrome(str.Substring(1, str.Length - 2));
+    //}
+
+    //static void Main()
+    //{
+    //    Console.Write("Введите число: ");
+    //    string str = Console.ReadLine();
+
+    //    if (Palindrome(str))
+    //        Console.WriteLine($"Число '{str}' является палиндромом");
+    //    else
+    //        Console.WriteLine($"Число '{str}' не является палиндромом");
+    //}
+
+    ////задача7
+    //static void Main()
+    //{
+    //    int numDisks = 3;
+    //    TowerOfHanoi(numDisks, 'A', 'C', 'B'); 
+    //}
+
+    //static void TowerOfHanoi(int n, char source, char target, char auxiliary)
+    //{
+    //    if (n == 1)
+    //    {
+    //        Console.WriteLine($"Переместите диск 1 с {source} на {target}");
+    //        return;
+    //    }
+
+    //    TowerOfHanoi(n - 1, source, auxiliary, target);
+
+    //    Console.WriteLine($"Переместите диск {n} с {source} на {target}");
+
+    //    TowerOfHanoi(n - 1, auxiliary, target, source);
+    //}
+
+    ////задача8
+    //static void Main()
+    //{
+    //    int[] set = { 1, 2, 3 };
+    //    List<int> currentSubset = new List<int>();
+
+    //    Console.WriteLine("Все подмножества:");
+    //    GenerateSubsets(set, currentSubset, 0);
+    //}
+
+    //static void GenerateSubsets(int[] set, List<int> currentSubset, int index)
+    //{
+    //    Console.WriteLine("{" + string.Join(", ", currentSubset) + "}");
+
+    //    for (int i = index; i < set.Length; i++)
+    //    {
+    //        currentSubset.Add(set[i]);
+
+    //        GenerateSubsets(set, currentSubset, i + 1);
+
+    //        currentSubset.RemoveAt(currentSubset.Count - 1);
+    //    }
+    //}
+
+    //задача9
+
+
+    //задача10
+    //static void Main()
+    //{
+    //    string input = "abc";
+    //    List<string> permutations = new List<string>();
+
+    //    GeneratePermutations(input.ToCharArray(), 0, permutations);
+
+    //    Console.WriteLine("Все перестановки:");
+    //    foreach (string perm in permutations)
+    //    {
+    //        Console.WriteLine(perm);
+    //    }
+    //}
+
+    //static void GeneratePermutations(char[] array, int index, List<string> result)
+    //{
+    //    if (index == array.Length - 1)
+    //    {
+    //        result.Add(new string(array)); // Добавляем перестановку в результат
+    //        return;
+    //    }
+
+    //    for (int i = index; i < array.Length; i++)
+    //    {
+    //        Swap(ref array[index], ref array[i]); // Меняем местами
+    //        GeneratePermutations(array, index + 1, result); // Рекурсивный вызов
+    //        Swap(ref array[index], ref array[i]); // Возврат в исходное состояние (backtrack)
+    //    }
+    //}
+
+    //static void Swap(ref char a, ref char b)
+    //{
+    //    char temp = a;
+    //    a = b;
+    //    b = temp;
+    //}
+}
---
+++ Course/2nd year/Lesson7/practice/practice_bc_lesson7.cs
@@ -0,0 +1,141 @@
+﻿////практика в и с
+////задание 11
+
+using System;
+
+class Program
+{
+    //static void Main()
+    //{
+    //    int[,] maze = {
+    //        { 0, 1, 0, 0, 0 },
+    //        { 0, 1, 0, 1, 0 },
+    //        { 0, 0, 0, 1, 0 },
+    //        { 0, 1, 0, 0, 0 },
+    //        { 0, 0, 0, 1, 0 }
+    //    };
+
+    //    if (FindPath(maze, 0, 0))
+    //    {
+    //        Console.WriteLine("Найден путь в лабиринте");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Путь не найден.");
+    //    }
+    //}
+
+    //static bool FindPath(int[,] maze, int x, int y)
+    //{
+    //    if (x == maze.GetLength(0) - 1 && y == maze.GetLength(1) - 1)
+    //    {
+    //        maze[x, y] = 2; // Отмечаем путь
+    //        PrintMaze(maze);
+    //        return true;
+    //    }
+
+    //    if (IsSafe(maze, x, y))
+    //    {
+    //        maze[x, y] = 2;
+
+    //        if (FindPath(maze, x + 1, y)) // Вниз
+    //            return true;
+    //        if (FindPath(maze, x, y + 1)) // Вправо
+    //            return true;
+    //        if (FindPath(maze, x - 1, y)) // Вверх
+    //            return true;
+    //        if (FindPath(maze, x, y - 1)) // Влево
+    //            return true;
+
+    //        maze[x, y] = 0;
+    //    }
+
+    //    return false; 
+    //}
+
+    //static bool IsSafe(int[,] maze, int x, int y)
+    //{
+    //    return (x >= 0 && x < maze.GetLength(0) &&
+    //            y >= 0 && y < maze.GetLength(1) &&
+    //            maze[x, y] == 0);
+    //}
+
+    //static void PrintMaze(int[,] maze)
+    //{
+    //    Console.WriteLine("Текущий лабиринт:");
+    //    for (int i = 0; i < maze.GetLength(0); i++)
+    //    {
+    //        for (int j = 0; j < maze.GetLength(1); j++)
+    //        {
+    //            if (maze[i, j] == 2)
+    //                Console.Write(" * ");
+    //            else
+    //                Console.Write(" " + maze[i, j] + " ");
+    //        }
+    //        Console.WriteLine();
+    //    }
+    //    Console.WriteLine();
+    //}
+
+    ////задание12
+
+    //static void Main()
+    //{
+    //    int[] array = { 34, 7, 23, 32, 5, 62 };
+
+    //    Console.WriteLine("Исходный массив:");
+    //    PrintArray(array);
+
+    //    QuickSort(array, 0, array.Length - 1);
+
+    //    Console.WriteLine("Отсортированный массив:");
+    //    PrintArray(array);
+    //}
+
+    //static void QuickSort(int[] array, int low, int high)
+    //{
+    //    if (low < high)
+    //    {
+    //        // Находим опорный элемент и разбиваем массив
+    //        int pivotIndex = Partition(array, low, high);
+
+    //        // Рекурсивно сортируем элементы перед и после опорного элемента
+    //        QuickSort(array, low, pivotIndex - 1);
+    //        QuickSort(array, pivotIndex + 1, high);
+    //    }
+    //}
+
+    //static int Partition(int[] array, int low, int high)
+    //{
+    //    int pivot = array[high]; // Выбираем опорный элемент (последний элемент)
+    //    int i = low - 1; // Индекс меньшего элемента
+
+    //    for (int j = low; j < high; j++)
+    //    {
+    //        // Если текущий элемент меньше или равен опорному
+    //        if (array[j] <= pivot)
+    //        {
+    //            i++; // Увеличиваем индекс меньшего элемента
+    //            Swap(array, i, j);
+    //        }
+    //    }
+    //    Swap(array, i + 1, high); // Помещаем опорный элемент на правильное место
+    //    return i + 1; // Возвращаем индекс опорного элемента
+    //}
+
+    //static void Swap(int[] array, int i, int j)
+    //{
+    //    int temp = array[i];
+    //    array[i] = array[j];
+    //    array[j] = temp;
+    //}
+
+    //static void PrintArray(int[] array)
+    //{
+    //    foreach (int value in array)
+    //    {
+    //        Console.Write(value + " ");
+    //    }
+    //    Console.WriteLine();
+    //}
+}
